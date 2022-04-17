c65531f4326df15c22f295454e657d4c7e863c32
tavoludra1 <tavoludra@hotmail.com>
2022-04-17 17:42:01 -0500

    Initial Commit

---
+++ Xcode01/Xcode01App.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode01App.swift
+//  Xcode01
+//
+//  Created by GAPT on 17/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Xcode01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
